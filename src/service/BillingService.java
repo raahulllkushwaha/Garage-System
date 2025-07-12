dab2e03bde4f5c47b12ecdb95c84903056eee746
Rahul Kushwaha <raahullkushwaha@gmail.com>
2025-07-12 23:16:23 +0530

    Billing Service Program and generate Invoice

---
+++ src/service/BillingService.java
@@ -0,0 +1,26 @@
+package service;
+
+import entity.Customer;
+import entity.Invoice;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class BillingService {
+    public CustomerService customerService = new CustomerService();
+    public InvoiceService invoiceService = new InvoiceService();
+
+    public void createInvoice(int customerId, int vehicleId, List<Integer> serviceIds) throws SQLException {
+        for(int serviceId:serviceIds){
+            invoiceService.addInvoice(new Invoice(0, customerId, vehicleId, serviceId));
+        }
+        System.out.println("Invoice generated successfully!");
+    }
+
+    public void showAllInvoices() throws SQLException {
+        List<Invoice> invoices = invoiceService.getAllInvoices();
+        for(Invoice invoice: invoices){
+            System.out.println(invoice);
+        }
+    }
+}
