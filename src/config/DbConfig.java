7204f906a59c93adda5cca66faba37d943341eb7
Rahul Kushwaha <raahullkushwaha@gmail.com>
2025-07-12 22:04:28 +0530

    Setup DB in config pacakage

---
+++ src/config/DbConfig.java
@@ -0,0 +1,7 @@
+package config;
+
+public class DbConfig {
+    private static final String URL = "jdbc:mysql://localhost:3306/garage";
+    private static final String username = "root";
+    private static final String password = "123456";
+}
