13365d2c1b687df5c2b931bcb9c1415ba9f1d81b
Rahul Kushwaha <raahullkushwaha@gmail.com>
2025-07-12 22:22:16 +0530

    Throw Exception

--- src/config/DbConfig.java
+++ src/config/DbConfig.java
@@ -1,7 +1,15 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/garage";
-    private static final String username = "root";
-    private static final String password = "123456";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "123456";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
